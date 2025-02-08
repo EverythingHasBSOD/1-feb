383ea4adb634f615efe3252cd165f9af8c5eccca
EverythingHasBSOD <qasimlimurad92@gmail.com>
2025-02-08 22:07:57 +0400

    ubtate everything

---
+++ app.js
@@ -0,0 +1,21 @@
+let var_01 = 14
+let var_02 = 4
+let var_03_result = 0
+
+function func_plus() {  
+    var_03_result = var_01 + var_02
+document.getElementById("text_res").innerHTML = var_03_result}
+
+function func_minus() {  
+    var_03_result = var_01 - var_02
+document.getElementById("text_res").innerHTML = var_03_result}
+
+function func_times() {  
+    var_03_result = var_01 * var_02
+document.getElementById("text_res").innerHTML = var_03_result}
+
+function func_divede() {  
+    var_03_result = var_01 / var_02
+document.getElementById("text_res").innerHTML = var_03_result}
+    
+  
