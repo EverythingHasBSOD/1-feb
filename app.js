fa32a8b4e3445f59bef686436614caa85d7f9625
EverythingHasBSOD <qasimlimurad92@gmail.com>
2025-02-10 22:07:23 +0400

    msg_deploy now

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-let var_01 = 14
-let var_02 = 4
-let var_03_result = 0
+// varibles
+let var_01 = 14;
+let var_02 = 4;
+let var_03_result = 0;
 
+// function add
 function func_plus() {  
     var_03_result = var_01 + var_02
-document.getElementById("text_res").innerHTML = var_03_result}
+document.getElementById("text_res").innerHTML = "Result is "+var_03_result;
+console.log(var_03_result)}
 
+// function subtract
 function func_minus() {  
     var_03_result = var_01 - var_02
-document.getElementById("text_res").innerHTML = var_03_result}
+document.getElementById("text_res").innerHTML = "Result is "+var_03_result;;
+console.log(var_03_result)}
 
+// function multiply
 function func_times() {  
     var_03_result = var_01 * var_02
-document.getElementById("text_res").innerHTML = var_03_result}
+document.getElementById("text_res").innerHTML = "Result is "+var_03_result;;
+console.log(var_03_result)}
 
-function func_divede() {  
+// function devide
+function func_devide() {  
     var_03_result = var_01 / var_02
-document.getElementById("text_res").innerHTML = var_03_result}
+document.getElementById("text_res").innerHTML = "Result is "+var_03_result;;
+console.log(var_03_result)}
     
   
